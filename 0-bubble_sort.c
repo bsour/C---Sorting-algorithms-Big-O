c1cdbbaf3ba12e6eb209cb960dd8248aed5decac
EvaMicich <6366@holbertonstudents.com>
2023-05-17 17:45:41 -0700

    added code for bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ * swap_int - swaps integer
+ * @a: first int
+ * @b: second int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sort from lowest to highest int
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (array[j] > array[j + 1 ])
+			{
+				swap_int(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+}
